e07b96c24653d3811eb04c1c94cfb9ba9e01c9c1
planna_b <bounkhavy.plannavong@epitech.eu>
2018-03-17 20:21:20 +0100

    generate entity from DB

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,8 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new Crowding\ApiBundle\ApiBundle(),
+            new FOS\RestBundle\FOSRestBundle(),
+
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
